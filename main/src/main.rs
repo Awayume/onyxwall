4dddddef08d48d824e66cd1f802d3930c128e977
Awayume <dev@awayume.jp>
2023-06-11 15:50:27 +0900

    add: Print version message

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2023 Awayume <dev@awayume.jp>
+// SPDX-License-Identifier: APGL-3.0-only
+
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    println!("onyxwall (Version {})", VERSION);
 }
